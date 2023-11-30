f322ecff3ba871149b7e7a618d4fdc7cb645c1f9
sunhyeok26 <112845070+sunhyeok26@users.noreply.github.com>
2023-11-30 23:16:21 +0900

    add: best item, item hover, link

---
+++ js/item.js
@@ -0,0 +1,37 @@
+let item = [
+    {id : 0, type: 1, name: "[홀리데이] 섀도우 팔레트 + 립글로즈 SET", 
+    product: "https://dasique.co.kr/web/product/big/202311/37a7bb0616c8c820e24ce53ed07ac8f3.jpg",
+    model: "https://dasique.co.kr/web/product/medium/202311/a28556370c88a937bb24348d5d25b45b.jpg", 
+    price: "51,000원", sale: "29,900원",
+    state1: "https://dasique.co.kr/web/upload/icon_202205301503565000.png", // sold out
+    state2: "https://dasique.co.kr/web/upload/custom_4016538877427486.png", // new
+    state3: ""
+    },
+
+    {id : 1, type: 6, name: "[홀리데이 컬렉션] 쥬시 듀이 틴트",
+    product: "https://dasique.co.kr/web/product/big/202311/7a426d2ab5f05744117cbe54cd9e99da.jpg",
+    model: "https://dasique.co.kr/web/product/medium/202311/2bbdc2e6369427348283c602222e79ad.jpg",
+    price: "17,000원", sale: "11,900원",
+    state1: "https://dasique.co.kr/web/upload/custom_4016538877427486.png", // new
+    state2: "https://dasique.co.kr/web/upload/custom_4116538879977934.png", // sale
+    state3: ""
+    },
+
+    {id : 2, type: 7, name: "[넛츠 컬렉션] 프루티 립 잼",
+    product: "https://dasique.co.kr/web/product/big/202311/eb173d26652e6d8fc2c10d7fd8b484d4.jpg",
+    model: "https://dasique.co.kr/web/product/medium/202310/60eb8ec33e4933c5dbb9279cded3eb91.jpg",
+    price: "17,000원", sale: "11,900원",
+    state1: "https://dasique.co.kr/web/upload/icon_202205301503565000.png", // sold out
+    state2: "https://dasique.co.kr/web/upload/custom_4016538877427486.png", // new
+    state3: "https://dasique.co.kr/web/upload/custom_4116538879977934.png", // sale
+    },
+
+    {id : 3, type: 1, name: "[NEW] 섀도우 팔레트 24 뮤티드 넛츠",
+    product: "https://dasique.co.kr/web/product/big/202311/eb173d26652e6d8fc2c10d7fd8b484d4.jpg",
+    model: "https://dasique.co.kr/web/product/medium/202310/60eb8ec33e4933c5dbb9279cded3eb91.jpg",
+    price: "34,000원", sale: "27,200원",
+    state1: "https://dasique.co.kr/web/upload/custom_4016538877427486.png", // new
+    state2: "https://dasique.co.kr/web/upload/custom_4616538884112848.png", // 20%
+    state3: ""
+    }
+];
